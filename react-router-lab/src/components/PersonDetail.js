dd9cc5c596f28ba751f025d3f2c191ab4643c714
suman492 patro <suman.pat49@gmail.com>
2024-10-04 06:23:11 +0000

    added styling

--- react-router-lab/src/components/PersonDetail.js
+++ react-router-lab/src/components/PersonDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/PersonDetail.css'
 
 const PersonDetail = () => {
 
--- react-router-lab/src/components/PersonList.js
+++ react-router-lab/src/components/PersonList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import '../styles/PersonList.css'
 
 const PersonList = () => {
     const output = axios.get("https://5000-suman492-reactprojects-aml9r2kzmrb.ws-us116.gitpod.io/persons")
@@ -9,7 +10,7 @@ const PersonList = () => {
     const API_URL= process.env.REACT_APP_API_URL
     console.log(API_URL)
     return (
-        <div>
+        <div className='person-list'>
             <h2>personlist</h2>
         </div>
     )
